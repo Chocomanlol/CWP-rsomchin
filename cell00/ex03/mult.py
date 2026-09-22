d59274722420fc74a5438ce2314b8e12881e5d9d
Chocomanlol <rungruj8416@gmail.com>
2026-09-23 01:44:12 +0700

    gr

--- cell00/ex03/mult.py
+++ cell00/ex03/mult.py
@@ -1,11 +1,13 @@
-print("Enter the first number : ")
-num1 = int(input())
-print("Enter the second number : ")
-num2 = int(input())
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
 
-if num1 * num2 > 0:
+product = num1 * num2
+
+print(f"{num1} x {num2} = {product}")
+
+if product > 0:
     print("The result is positive.")
-elif num1 * num2 < 0:
+elif product < 0:
     print("The result is negative.")
 else:
-    print("The result is positive and negative")
+    print("The result is positive and negative.")
